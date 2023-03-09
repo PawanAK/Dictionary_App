e8ae91144da3437440f39612dbf365eb4815f32e
Pawan Ajjar K <pawanajjark@gmail.com>
2023-03-09 12:46:32 +0530

    only the meaning section left

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 'use strict'
 
-const word=document.querySelector('.search-field').value;
+
+const request=async function(word){
+    const search = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const data = await search.json();
+    render(data[0])
+}
+
+
+const render= function(data){
+    console.log(data);
+    render_name(data.word)
+    render_phonetics(data.phonetic)
+    render_audio(data.phonetics[0])
+    console.log(data.meanings[0].definitions[0]);
+}
+
+const render_name =function(name)
+{
+    document.querySelector('.word__english').innerHTML=name;
+}
+
+const render_phonetics = function(phone)
+{
+    document.querySelector('.word__phone').innerHTML=phone;
+}
+
+const render_audio = function(data)
+{
+    const audio = new Audio(data.audio)
+    const butt=document.querySelector('.play__audio');
+    butt.addEventListener("click",()=>audio.play())
+}
+
+
+
+
+
+
+
+
+
+const word = document.querySelector(".search-field").value;
 console.log(word);
-const search = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-console.log(search.json());
+
+request(word);
+
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttons = document.querySelectorAll("button");
+
