a04a7bf233386edf56781d3efee1fa1f5ee13c3c
Pawan Ajjar K <pawanajjark@gmail.com>
2023-03-09 15:26:11 +0530

    adding information like meaning done , css left o

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const render= function(data){
     render_name(data.word)
     render_phonetics(data.phonetic)
     render_audio(data.phonetics[0])
-    console.log(data.meanings[0].definitions[0]);
+    render__answer(data.meanings)
 }
 
 const render_name =function(name)
@@ -28,16 +28,32 @@ const render_phonetics = function(phone)
 
 const render_audio = function(data)
 {
+    console.log(data.audio);
     const audio = new Audio(data.audio)
     const butt=document.querySelector('.play__audio');
     butt.addEventListener("click",()=>audio.play())
 }
 
+const render__answer = function(data)
+{
+    let html='';
+    console.log(data);
 
+    data.forEach(data => {
+        const pof=data.partOfSpeech;
+        const def=data.definitions[0].definition;
+        html += `<div class="ans">
+    <div class="part">${pof}</div>
+    <div class="meaning">Meaning
+        <ul class="define">
+            <li class="defination">${def}</li>
+        </ul>
+    </div>
+</div> <br>`;
 
-
-
-
+document.querySelector('.answer').insertAdjacentHTML("afterend",html);
+    });
+}
 
 
 
@@ -49,3 +65,12 @@ request(word);
 const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 const buttons = document.querySelectorAll("button");
 
+`<div class="ans">
+    <div class="part">${data.partofSpeech}</div>
+    <div class="meaning">Meaning
+        <ul class="define">
+            <li class="defination">${data.definations[0].defination}</li>
+            <li class="defination">${data.definations[1].defination}</li>
+        </ul>
+    </div>
+</div>`
